c58983249164ae0f2d1fcd7574977f4f9f1960e7
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-11-29 16:10:35 +0400

    last dynamic version with little bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import construct = Reflect.construct;
+
 
 
 @Component({
@@ -14,7 +14,7 @@ export class AppComponent {
       itemName: 'products',
       dropdownData: [
         {
-          headline: 'ACCEPT PAYMENTS',
+          headline: 'accept payments',
           products: [
             {
               iconUrl: 'https://stripe.com/favicon.ico',
@@ -34,7 +34,7 @@ export class AppComponent {
           ]
         },
         {
-          headline: 'BUSINESS MODELS',
+          headline: 'business model',
           products: [
             {
               iconUrl: 'https://stripe.com/favicon.ico',
@@ -57,111 +57,52 @@ export class AppComponent {
     },
     {
       itemName: 'developers',
-      dropDownData: []
+      dropdownData: [
+        {
+          headline: 'accept payments',
+          products: [
+            {
+              iconUrl: 'https://stripe.com/favicon.ico',
+              title: 'Payments',
+              description: 'Full platform for online payments'
+            },
+            {
+              iconUrl: 'https://stripe.com/favicon.ico',
+              title: 'Terminal',
+              description: 'Programmable in-person payments'
+            }
+          ]
+        }
+      ]
     },
     {
       itemName: 'company',
-      dropDownData: []
+      dropdownData: [
+        {
+          headline: 'business model',
+          products: [
+            {
+              iconUrl: 'https://stripe.com/favicon.ico',
+              title: 'Billing',
+              description: 'Smart invoicing & subscription management'
+            },
+            {
+              iconUrl: 'https://stripe.com/favicon.ico',
+              title: 'Connect',
+              description: 'Multi-party payments for platforms & marketplaces'
+            },
+            {
+              iconUrl: 'https://stripe.com/favicon.ico',
+              title: 'Radar',
+              description: 'Fraud prevention with machine learning'
+            }
+          ]
+        }
+      ]
     }
-  ];
-
-  dropdownData = [
-    // left vertical side (white background)
-    [
-      {
-        headline: 'ACCEPT PAYMENTS',
-        products: [
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Payments',
-            description: 'Full platform for online payments'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Terminal',
-            description: 'Programmable in-person payments'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Radar',
-            description: 'Fraud prevention with machine learning'
-          }
-        ]
-      },
-      {
-        headline: 'BUSINESS MODELS',
-        products: [
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Billing',
-            description: 'Smart invoicing & subscription management'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Connect',
-            description: 'Multi-party payments for platforms & marketplaces'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Radar',
-            description: 'Fraud prevention with machine learning'
-          }
-        ]
-      }
-    ],
-
-    // right vertical side (gray background)
-    [
-      {
-        headline: 'MOVE MONEY',
-        products: [
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Payouts',
-            description: 'Fast payouts for global platforms'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Issuing',
-            description: 'Toolkit for creating & managing cards'
-          }
-        ]
-      },
-      {
-        headline: 'FINANCIAL SERVICES',
-        products: [
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Corporate Card',
-            description: 'Corporate cards & expense management'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Capital',
-            description: 'Flexible financing in as little as one day'
-          }
-        ]
-      },
-      {
-        headline: 'BUSINESS OPERATIONS',
-        products: [
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Atlas',
-            description: 'Incorporation for startups'
-          },
-          {
-            iconUrl: 'https://stripe.com/favicon.ico',
-            title: 'Sigma',
-            description: 'Advanced business analytics & reporting'
-          }
-        ]
-      }
-    ]
   ];
 
 
   constructor() {
-
   }
 }
--- src/app/msda-navigation-with-dropdown/msda-navigation-with-dropdown.component.ts
+++ src/app/msda-navigation-with-dropdown/msda-navigation-with-dropdown.component.ts
@@ -1,5 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-
+import {AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 
 
 
@@ -8,7 +7,7 @@ import {AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChil
   templateUrl: './msda-navigation-with-dropdown.component.html',
   styleUrls: ['./msda-navigation-with-dropdown.component.scss']
 })
-export class MsdaNavigationWithDropdownComponent implements OnInit, AfterViewInit {
+export class MsdaNavigationWithDropdownComponent implements OnInit, AfterViewInit, OnChanges {
 
   @ViewChildren('navItemRef') navItemRefs: QueryList<any>;
   @ViewChildren('sectionElRef') sectionElRefs: QueryList<any>;
@@ -23,155 +22,84 @@ export class MsdaNavigationWithDropdownComponent implements OnInit, AfterViewIni
 
 
   // TODO: generate on the fly
-  // dimensions = {
-  //   products: {width: 490, height: 280, x: 0},
-  //   developers: {width: 390, height: 266, x: 100},
-  //   company: {width: 260, height: 296, x: 200}
-  // };
   dimensions = [
     {width: 490, height: 280, x: 0},
     {width: 390, height: 266, x: 100},
     {width: 260, height: 296, x: 200}
   ];
 
-  allDimentions = [];
-
-  firstSectionHeight;
-  firstSectionWidth;
-  firstSectionXPosition;
-
-  activeSectionHeight;
-  activeSectionWidth;
-  activeSectionXPosition;
-
-
-  // navigation = [
-  //   {name: 'navitem1', dropdownContent: 'dropdown content 1'},
-  //   {name: 'navitem2', dropdownContent: 'dropdown content 2'},
-  //   {name: 'navitem3', dropdownContent: 'dropdown content 3'}
-  // ];
 
   @Input() public navigationData;
 
 
-
-  showSection(section, index) {
+  showSection(index) {
     this.popoverElRef.nativeElement.classList.add('open');
     this.sectionElRefs.forEach(el => el.nativeElement.classList.remove('active'));
-    this.sectionElRefs.forEach((el, i) => {
-      // if (i === 0) {
-      //   // console.log('========', el.nativeElement.getBoundingClientRect());
-      //   this.firstSectionHeight = el.nativeElement.getBoundingClientRect().height;
-      //   this.firstSectionWidth = el.nativeElement.getBoundingClientRect().width;
-      //   this.firstSectionXPosition = el.nativeElement.getBoundingClientRect().x;
-      // }
 
-      // console.log('el!!!!!!!1', el.nativeElement.getBoundingClientRect());
-      // this.dimensions[index]['height'] = el.nativeElement.getBoundingClientRect().height;
-      // this.dimensions[index]['width'] = el.nativeElement.getBoundingClientRect().width;
+    this.sectionElRefs.forEach((el, i) => {
+
+      // this.dimensions[i]['height'] = el.nativeElement.offsetHeight;
+      // this.dimensions[i]['width'] = el.nativeElement.offsetWidth;
       // this.dimensions[index]['x'] = el.nativeElement.getBoundingClientRect().x;
-      console.log(this.dimensions);
 
       if (index === i) {
         el.nativeElement.classList.add('active');
-
-        // activeSectionHeight = el.nativeElement.offsetHeight;
-        // this.activeSectionHeight = el.nativeElement.getBoundingClientRect().height;
-        // activeSectionWidth = el.nativeElement.offsetWidth;
-        // this.activeSectionWidth = el.nativeElement.getBoundingClientRect().width;
-        // this.activeSectionXPosition = el.nativeElement.getBoundingClientRect().x;
-
-        // console.log('active element:', el.nativeElement.getBoundingClientRect());
-
       }
     });
 
 
-
-    // console.log(dimensions[section], dimensions['products']);
-
-
-    // this.sectionElRefs[index].nativeElement.classList.add('active');
-
     // Position arrow
-    // this.arrowElRef.nativeElement['style'].transform = `translateX(${this.dimensions[section].arrowX}px)rotate(45deg)`;
     this.arrowElRef.nativeElement['style'].transform = `translateX(${this.dimensions[index]['arrowX']}px)rotate(45deg)`;
 
     // Resize and position background
-    // this.backgroundElRef.nativeElement['style'].transform = `
-    //   translateX(${this.dimensions[section].x}px)
-    //   scaleX(${this.dimensions[section].width / this.dimensions['products'].width})
-    //   scaleY(${this.dimensions[section].height / this.dimensions['products'].height})
-    // `;
-    // this.backgroundElRef.nativeElement['style'].transform = `
-    //   translateX(${this.dimensions[index].x}px)
-    //   scaleX(${this.dimensions[index].width / this.dimensions['products'].width})
-    //   scaleY(${this.dimensions[index].height / this.dimensions['products'].height})
-    // `;
     this.backgroundElRef.nativeElement['style'].transform = `
       translateX(${this.dimensions[index].x}px)
       scaleX(${this.dimensions[index].width / this.dimensions[0].width})
       scaleY(${this.dimensions[index].height / this.dimensions[0].height})
     `;
-    // this.backgroundElRef.nativeElement['style'].transform = `
-    //   translateX(${this.activeSectionXPosition}px)
-    //   scaleX(${this.activeSectionWidth / this.firstSectionWidth})
-    //   scaleY(${this.activeSectionHeight / this.firstSectionHeight})
-    // `;
-    // this.backgroundElRef.nativeElement.style.height = this.dimensions[index].height + 'px';
+
+    this.backgroundElRef.nativeElement.style.height = this.dimensions[index].height + 'px';
     // this.backgroundElRef.nativeElement.style.width = this.dimensions[index].width + 'px';
 
-
-    this.backgroundElRef.nativeElement.style.backgroundColor = 'red';
-
     // Resize and position content
-    // this.contentElRef.nativeElement.style.width = this.dimensions[section].width + 'px';
     this.contentElRef.nativeElement.style.width = this.dimensions[index].width + 'px';
-    // this.contentElRef.nativeElement.style.width = this.activeSectionWidth + 'px';
-    // this.contentElRef.nativeElement.style.height = this.dimensions[section].height + 'px';
     this.contentElRef.nativeElement.style.height = this.dimensions[index].height + 'px';
-    // this.contentElRef.nativeElement.style.height = this.activeSectionHeight + 'px';
-    // this.contentElRef.nativeElement.style.transform = `translateX(${this.dimensions[section].x}px)`;
     this.contentElRef.nativeElement.style.transform = `translateX(${this.dimensions[index].x}px)`;
-    // this.contentElRef.nativeElement.style.transform = `translateX(${this.activeSectionXPosition}px)`;
-
-    // size container? If we remove from CSS and do everything dynamically.
   }
-
 
 
   constructor() {
   }
 
   ngOnInit() {
+  }
 
+  ngOnChanges() {
+    this.navigationData.forEach((navItem, i) => {
+      let numberOfProducts = 0;
+      navItem.dropdownData.forEach(dropdownItem => {
+        numberOfProducts += dropdownItem.products.length;
+      });
+
+      console.log(i, numberOfProducts);
+      this.dimensions[i]['height'] = numberOfProducts * 50 + 250;
+    });
   }
 
   ngAfterViewInit() {
-    this.popoverLeft = this.popoverElRef.nativeElement.getBoundingClientRect()['x'];
-
-
+    this.popoverLeft = this.popoverElRef.nativeElement.getBoundingClientRect().x;
 
     this.navItemRefs.forEach((navItem, i) => {
       const section = navItem.nativeElement.innerText;
       const rect = navItem.nativeElement.getBoundingClientRect();
-
-      // dimensions[section].arrowX = rect.left + (rect.width / 2) - this.popoverLeft;
-      // this.dimensions[section].arrowX = rect.left + (rect.width / 2) - this.popoverLeft;
       this.dimensions[i]['arrowX'] = rect.left + (rect.width / 2) - this.popoverLeft;
     });
 
-
-
     // Set initial arrow position
-    // this.arrowElRef.nativeElement['style'].transform = `translateX(${this.dimensions.products['arrowX']}px)rotate(45deg)`;
     this.arrowElRef.nativeElement['style'].transform = `translateX(${this.dimensions[0]['arrowX']}px)rotate(45deg)`;
-
 
     this.headerElRef.nativeElement.addEventListener('mouseleave', () => {
       this.popoverElRef.nativeElement.classList.remove('open');
     });
-
   }
-
 }
